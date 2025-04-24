3dbac12c11e84ea4e814530d45e79b0ea993ef26
Obaida Mohammed <76841426+obaida09@users.noreply.github.com>
2025-04-24 17:59:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// Animation Script 
+{/* <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Progress bars animation
+    document.querySelectorAll('.animate-progress').forEach(el => {
+      const width = el.getAttribute('data-width');
+      el.style.setProperty('--width', width);
+    });
+
+        // Count up animation
+        document.querySelectorAll('.animate-count-up').forEach(el => {
+            const target = el.getAttribute('data-target');
+  const countUp = new CountUp(el, target, {duration: 2 });
+  countUp.start();
+        });
+    });
+</script> */}
